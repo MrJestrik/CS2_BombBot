ddf9ecaf8073adf572f5916cf9de91d338cffb6b
Mr.Jestrik The Jester <legend.gm3121@gmail.com>
2023-11-12 23:50:09 +0800

    Update main.py
    fixed some minor issues.

--- main.py
+++ main.py
@@ -12,27 +12,32 @@ def main():
     client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
 
     while True:
-
-        cplantedc4 = pm.read_ulonglong(client + plantedC4)
-        cplantedc4 = pm.read_ulonglong(cplantedc4)
-        planted = pm.read_bool(client + plantedC4 - 0x8)
-        if planted:
-            site = pm.read_int(cplantedc4 + m_nBombSite)
-            beingdefused = pm.read_bool(cplantedc4 + m_bBeingDefused)
-            defused = pm.read_bool(cplantedc4 + m_bBombDefused)
-            # cplantedc4 = pm.read_ulonglong(client + plantedC4)
-            if site > 0:
-                site = "B"
+        try:
+            cplantedc4 = pm.read_ulonglong(client + plantedC4)
+            cplantedc4 = pm.read_ulonglong(cplantedc4)
+            planted = pm.read_bool(client + plantedC4 - 0x8)
+            if planted:
+                site = pm.read_int(cplantedc4 + m_nBombSite)
+                beingdefused = pm.read_bool(cplantedc4 + m_bBeingDefused)
+                defused = pm.read_bool(cplantedc4 + m_bBombDefused)
+                # cplantedc4 = pm.read_ulonglong(client + plantedC4)
+                if site > 0:
+                    site = "B"
+                else:
+                    site = "A"
+                if not defused:
+                    print(f"Bomb planted on: {site}\nDefusing: {beingdefused}")
+                else:
+                    print("Bomb Defused!")
             else:
-                site = "A"
-            if not defused:
-                print(f"Bomb planted on: {site}\nDefusing: {beingdefused}")
-            else:
-                print("Bomb Defused!")
-        else:
-            print("Bomb not planted!")
+                print("Bomb not planted!")
 
-        time.sleep(1)
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+            break
+        except:
+            pass
 
 if __name__ == "__main__":
     main()
